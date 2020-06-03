a160991570aef1a85dcb47d01f6e904ec7b69cc7
Chaser <904762746@qq.com>
2020-06-03 21:33:25 +0800

    修改传参方式

--- interface.cpp
+++ interface.cpp
@@ -8,7 +8,7 @@ int Interface::vic[6][5]={ {0,0,1,0,0},
                 {0,1,0,1,0},
                 {0,0,1,0,0},
                 {0,0,1,0,0}};
-void Interface::initialWindow(HANDLE hOut)
+void Interface::initialWindow()
 {
     COORD size = {100, 30};
     SetConsoleScreenBufferSize(hOut, size);//更改指定缓冲区大小
@@ -18,24 +18,24 @@ void Interface::initialWindow(HANDLE hOut)
     SetConsoleCursorInfo(hOut, &cursor_info);//设置光标不可见
     SetConsoleTitle("我罗斯");//设置控制台窗口标题
 }
-void Interface::gotoXY(HANDLE hOut, int x, int y){
+void Interface::gotoXY(int x, int y){
     COORD pos;
     pos.X = x;
     pos.Y = y;
     SetConsoleCursorPosition(hOut, pos);//设置光标位置
 }
-void Interface::initialViewOnePlayer(HANDLE hOut){
+void Interface::initialViewOnePlayer(){
     SetConsoleTextAttribute(hOut,15);//设置为白色
     for(int i=0;i<20;i++){
         cout<<"                    ■                    ■"<<endl;
     }
-    gotoXY(hOut,0,20);
+    gotoXY(0,20);
     cout<<"                    ■■■■■■■■■■■■"<<endl;
-    gotoXY(hOut,0,0);
+    gotoXY(0,0);
     cout<<"■玩家："<<endl<<endl;
     cout<<"■分数："<<endl<<endl;
     cout<<"■下一个方块："<<endl;
-    gotoXY(hOut,0,11);
+    gotoXY(0,11);
     SetConsoleTextAttribute(hOut,3);
     cout<<"■提示："<<endl<<endl;
     cout<<"W：转换方块朝向"<<endl;
@@ -44,24 +44,24 @@ void Interface::initialViewOnePlayer(HANDLE hOut){
     cout<<"D：控制方块向右移动"<<endl;  
     cout<<"[ 空格 ]：暂停游戏"<<endl;
     cout<<"X：重新开始游戏 "<<endl;
-    gotoXY(hOut,0,23);
+    gotoXY(0,23);
     SetConsoleTextAttribute(hOut,12);
     cout<<"■■单人模式■■"<<endl;
     SetConsoleTextAttribute(hOut,15);
     cout<<"By 余佳硕 吕航 阙嘉毅"<<endl;
 }
-void Interface::initialViewTwoPlayer(HANDLE hOut){
+void Interface::initialViewTwoPlayer(){
     SetConsoleTextAttribute(hOut,15);//设置为白色
     for(int i=0;i<20;i++){
         cout<<"                    ■                    ■                     ■                    ■"<<endl;
     }
-    gotoXY(hOut,0,20);
+    gotoXY(0,20);
     cout<<"                    ■■■■■■■■■■■■                     ■■■■■■■■■■■■"<<endl;
-    gotoXY(hOut,0,0);
+    gotoXY(0,0);
     cout<<"■玩家1："<<endl<<endl;
     cout<<"■分数："<<endl<<endl;
     cout<<"■下一个方块："<<endl;
-    gotoXY(hOut,0,11);
+    gotoXY(0,11);
     SetConsoleTextAttribute(hOut,3);
     cout<<"■提示："<<endl<<endl;
     cout<<"W：转换方块朝向"<<endl;
@@ -71,71 +71,71 @@ void Interface::initialViewTwoPlayer(HANDLE hOut){
     cout<<"[ 空格 ]：暂停游戏"<<endl;
     cout<<"X：重新开始游戏 "<<endl;
     SetConsoleTextAttribute(hOut,15);
-    gotoXY(hOut,44,0);
+    gotoXY(44,0);
     cout<<"■玩家2："<<endl<<endl;
-    gotoXY(hOut,44,2);
+    gotoXY(44,2);
     cout<<"■分数："<<endl<<endl;
-    gotoXY(hOut,44,4);
+    gotoXY(44,4);
     cout<<"■下一个方块："<<endl;
     SetConsoleTextAttribute(hOut,14);
-    gotoXY(hOut,44,11);
+    gotoXY(44,11);
     cout<<"■提示：";
-    gotoXY(hOut,44,13);
+    gotoXY(44,13);
     cout<<"↑：转换方块朝向";
-    gotoXY(hOut,44,14);
+    gotoXY(44,14);
     cout<<"↓：加速方块下落";
-    gotoXY(hOut,44,15);
+    gotoXY(44,15);
     cout<<"←：控制方块向右移动";
-    gotoXY(hOut,44,16);
+    gotoXY(44,16);
     cout<<"→：控制方块向右移动";  
-    gotoXY(hOut,44,17);
+    gotoXY(44,17);
     cout<<"[ 空格 ]：暂停游戏";
-    gotoXY(hOut,44,18);
+    gotoXY(44,18);
     cout<<"X：重新开始游戏 "<<endl;
-    gotoXY(hOut,0,23);
+    gotoXY(0,23);
     SetConsoleTextAttribute(hOut,11);
     cout<<"■■双人模式■■"<<endl;
     SetConsoleTextAttribute(hOut,15);
     cout<<"By 余佳硕 吕航 阙嘉毅"<<endl;    
 }
-void Interface::printNamePlayer1(HANDLE hOut,string name){//显示玩家1名字
+void Interface::printNamePlayer1(string name){//显示玩家1名字
     SetConsoleTextAttribute(hOut,15);
-    gotoXY(hOut,9,0);                        
+    gotoXY(9,0);                        
     cout<<name<<endl;
      
 }
-void Interface::printNamePlayer2(HANDLE hOut,string name){//显示玩家2名字
+void Interface::printNamePlayer2(string name){//显示玩家2名字
     SetConsoleTextAttribute(hOut,15); 
-    gotoXY(hOut,53,0);                        
+    gotoXY(53,0);                        
     cout<<name<<endl;
 
 }
-void Interface::printPointPlayer1(HANDLE hOut,int point){ //显示玩家1分数
+void Interface::printPointPlayer1(int point){ //显示玩家1分数
     SetConsoleTextAttribute(hOut,15);
-    gotoXY(hOut,8,2);
+    gotoXY(8,2);
     cout<<"   "<<endl; 
-    gotoXY(hOut,8,2);                                  
+    gotoXY(8,2);                                  
     cout<<point<<endl;                         
 };
- void Interface::printPointPlayer2(HANDLE hOut,int point){ //显示玩家2分数
+ void Interface::printPointPlayer2(int point){ //显示玩家2分数
     SetConsoleTextAttribute(hOut,15);
-    gotoXY(hOut,52,2);
+    gotoXY(52,2);
     cout<<"   "<<endl; 
-    gotoXY(hOut,52,2);
+    gotoXY(52,2);
     cout<<point<<endl;
  }
-void Interface::player1Victory(HANDLE hOut){
-    gotoXY(hOut,26,22);
+void Interface::player1Victory(){
+    gotoXY(26,22);
     SetConsoleTextAttribute(hOut,12);
     for(int i=0;i<2;i++){
-        gotoXY(hOut,26,22+i);
+        gotoXY(26,22+i);
         for(int j=0;j<5;j++){
             if(vic[i][j]==0)cout<<"  ";
             else cout<<"■";
         }
         cout<<endl;
     }
-    gotoXY(hOut,26,24);
+    gotoXY(26,24);
     SetConsoleTextAttribute(hOut,14);
     cout<<"■";
     SetConsoleTextAttribute(hOut,12);
@@ -143,7 +143,7 @@ void Interface::player1Victory(HANDLE hOut){
     SetConsoleTextAttribute(hOut,14);
     cout<<"■"<<endl;
     for(int i=3;i<6;i++){
-        gotoXY(hOut,26,22+i);
+        gotoXY(26,22+i);
         for(int j=0;j<5;j++){
             if(vic[i][j]==0)cout<<"  ";
             else cout<<"■";
@@ -151,18 +151,18 @@ void Interface::player1Victory(HANDLE hOut){
         cout<<endl;
     }   
 }
-void Interface::player2Victory(HANDLE hOut){
-    gotoXY(hOut,71,22);
+void Interface::player2Victory(){
+    gotoXY(71,22);
     SetConsoleTextAttribute(hOut,12);
     for(int i=0;i<2;i++){
-        gotoXY(hOut,71,22+i);
+        gotoXY(71,22+i);
         for(int j=0;j<5;j++){
             if(vic[i][j]==0)cout<<"  ";
             else cout<<"■";
         }
         cout<<endl;
     }
-    gotoXY(hOut,71,24);
+    gotoXY(71,24);
     SetConsoleTextAttribute(hOut,14);
     cout<<"■";
     SetConsoleTextAttribute(hOut,12);
@@ -170,7 +170,7 @@ void Interface::player2Victory(HANDLE hOut){
     SetConsoleTextAttribute(hOut,14);
     cout<<"■"<<endl;
     for(int i=3;i<6;i++){
-        gotoXY(hOut,71,22+i);
+        gotoXY(71,22+i);
         for(int j=0;j<5;j++){
             if(vic[i][j]==0)cout<<"  ";
             else cout<<"■";
@@ -178,75 +178,75 @@ void Interface::player2Victory(HANDLE hOut){
         cout<<endl;
     }   
 }
-void Interface::drawNextBlock1(HANDLE hOut,int block[4][4],int color){
+void Interface::drawNextBlock1(int block[4][4],int color){
     for(int i=0;i<4;i++){
-        gotoXY(hOut,2,6+i);
+        gotoXY(2,6+i);
         for(int j=0;j<4;j++){
             cout<<"  ";
         }
     }
     SetConsoleTextAttribute(hOut,color);
     for(int i=0;i<4;i++){
-        gotoXY(hOut,2,6+i);
+        gotoXY(2,6+i);
         for(int j=0;j<4;j++){
             if(block[i][j]==1)cout<<"■";
             else cout<<"  ";
         }
     }
 };//画下一个方块
-void Interface::drawNextBlock2(HANDLE hOut,int block[4][4],int color){
+void Interface::drawNextBlock2(int block[4][4],int color){
     for(int i=0;i<4;i++){
-        gotoXY(hOut,46,6+i);
+        gotoXY(46,6+i);
         for(int j=0;j<4;j++){
             cout<<"  ";
         }
     }
     SetConsoleTextAttribute(hOut,color);
     for(int i=0;i<4;i++){
-        gotoXY(hOut,46,6+i);
+        gotoXY(46,6+i);
         for(int j=0;j<4;j++){
             if(block[i][j]==1)cout<<"■";
             else cout<<"  ";
         }
     }
 };
-void Interface::drawNowBlock1(HANDLE hOut,int block[4][4],int x,int y,int color){
+void Interface::drawNowBlock1(int block[4][4],int x,int y,int color){
     SetConsoleTextAttribute(hOut,color);
     for(int i=0;i<4;i++){
         for(int j=0;j<4;j++){
             if(block[i][j]==1){ 
-            gotoXY(hOut,2*(y+j)+22,x+i);
+            gotoXY(2*(y+j)+22,x+i);
             cout << "■";
             }
         }
     }
 };//画现在游戏池内的方块
-void Interface::drawNowBlock2(HANDLE hOut,int block[4][4],int x,int y,int color){
+void Interface::drawNowBlock2(int block[4][4],int x,int y,int color){
     SetConsoleTextAttribute(hOut,color);
     for(int i=0;i<4;i++){
         for(int j=0;j<4;j++){
             if(block[i][j]==1){ 
-            gotoXY(hOut,2*(y+j)+67,x+i);
+            gotoXY(2*(y+j)+67,x+i);
             cout << "■";
             }
         }
     }
 };
-void Interface::deleteBlock1(HANDLE hOut,int block[4][4],int x, int y){
+void Interface::deleteBlock1(int block[4][4],int x, int y){
     for(int i=0;i<4;i++){
         for(int j=0;j<4;j++){
             if(block[i][j]==1){ 
-            gotoXY(hOut,2*(y+j)+22,x+i);
+            gotoXY(2*(y+j)+22,x+i);
             cout <<"  ";
             }
         }
     }
 };//将游戏池中已打印方块消除
-void Interface::deleteBlock2(HANDLE hOut,int block[4][4],int x, int y){
+void Interface::deleteBlock2(int block[4][4],int x, int y){
     for(int i=0;i<4;i++){
         for(int j=0;j<4;j++){
             if(block[i][j]==1){ 
-            gotoXY(hOut,2*(y+j)+67,x+i);
+            gotoXY(2*(y+j)+67,x+i);
             cout <<"  ";
             }
         }
--- interface.h
+++ interface.h
@@ -3,25 +3,28 @@
 using namespace std;
 class Interface {
 public:
-    void initialWindow(HANDLE hOut);//初始化窗口
-    void initialViewOnePlayer(HANDLE hOut);//初始化map
-    void initialViewTwoPlayer(HANDLE hOut);//初始化map
-    void printPointPlayer1(HANDLE hOut,int point);//显示玩家1分数
-    void printPointPlayer2(HANDLE hOut,int point);//显示玩家2分数
-    void printNamePlayer1(HANDLE hOut,string name);//显示玩家1名字
-    void printNamePlayer2(HANDLE hOut,string name);//显示玩家2名字
-    void gotoXY(HANDLE hOut, int x, int y);//定位光标
-    void player1Victory(HANDLE hOut);//玩家胜利效果
-    void player2Victory(HANDLE hOut);//玩家胜利效果
-    void drawNextBlock1(HANDLE hOut,int block[4][4],int color);//画下一个方块
-    void drawNextBlock2(HANDLE hOut,int block[4][4],int color);
-    void drawNowBlock1(HANDLE hOut,int block[4][4],int x,int y,int color);//画现在游戏池内的方块
-    void drawNowBlock2(HANDLE hOut,int block[4][4],int x,int y,int color);
-    void deleteBlock1(HANDLE hOut,int block[4][4],int x, int y);//将游戏池中已打印方块消除
-    void deleteBlock2(HANDLE hOut,int block[4][4],int x, int y);
+    Interface(){
+       hOut = GetStdHandle(STD_OUTPUT_HANDLE);
+    }
+    void initialWindow();//初始化窗口
+    void initialViewOnePlayer();//初始化map
+    void initialViewTwoPlayer();//初始化map
+    void printPointPlayer1(int point);//显示玩家1分数
+    void printPointPlayer2(int point);//显示玩家2分数
+    void printNamePlayer1(string name);//显示玩家1名字
+    void printNamePlayer2(string name);//显示玩家2名字
+    void gotoXY(int x, int y);//定位光标
+    void player1Victory();//玩家胜利效果
+    void player2Victory();//玩家胜利效果
+    void drawNextBlock1(int block[4][4],int color);//画下一个方块
+    void drawNextBlock2(int block[4][4],int color);
+    void drawNowBlock1(int block[4][4],int x,int y,int color);//画现在游戏池内的方块
+    void drawNowBlock2(int block[4][4],int x,int y,int color);
+    void deleteBlock1(int block[4][4],int x, int y);//将游戏池中已打印方块消除
+    void deleteBlock2(int block[4][4],int x, int y);
     static int vic[6][5];
 private:
-
+    HANDLE hOut;
 };
 //单人模式和双人模式player1均调用1系列函数
 //双人模式player2调用2系列函数
